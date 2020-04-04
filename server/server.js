61ed7991646099707400771892216960851fe17c
Shan Wong <shan.wong29@gmail.com>
2020-04-04 08:34:08 +0200

    seperate server side for better structure

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 
-app.use(cors());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ["http://localhost:9000"] // <== this will be the URL of our React app (it will be running on port 3000)
+//   })
+// );
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
 
